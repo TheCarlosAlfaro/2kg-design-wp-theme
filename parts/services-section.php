a07dce0a1a377ef756d2edc916d0ffdf6099a436
devbysalas <rgkalin@gmail.com>
2018-12-27 19:22:21 -0800

    Tiles kinda working

--- parts/services-section.php
+++ parts/services-section.php
@@ -19,32 +19,44 @@ if (!defined('ABSPATH')) {
     <div class="row">
       
       <div class="col-md mx-auto services__tile-1">
-        <div class="container pt-5">
-          <h3 class="text-center display-4">TILE 1</h3>
+        <div class="text-overlay__container">
+          <!-- <h3 class="text-center display-4">TILE 1</h3> -->
+          <img src="http://2kg-design.local/wp-content/uploads/2018/12/branding.jpg" alt="" class="img-res shadow text-overlay__image">
         </div>
-        
+        <div class="text-overlay__overlay overlayLeft">
+            <div class="text-overlay__text text-center"><?php the_title(); ?></div>
+          </div>
       </div>
       <div class="col-md mx-auto services__tile-2">
-        <div class="container pt-5">
-          <h3 class="text-center display-4">TILE 2</h3>
-          
+        <div class="text-overlay__container">
+          <!-- <h3 class="text-center display-4">TILE 2</h3> -->
+          <img src="http://2kg-design.local/wp-content/uploads/2018/12/branding.jpg" alt="" class="img-res shadow text-overlay__image">
         </div>
+        <div class="text-overlay__overlay overlayLeft">
+            <div class="text-overlay__text text-center"><?php the_title(); ?></div>
+          </div>
       </div>
       
     </div> 
     <div class="row">
       
       <div class="col-md mx-auto services__tile-3">
-        <div class="container pt-5">
-          <h3 class="text-center display-4">TILE 3</h3>
-          
+        <div class="text-overlay__container">
+          <!-- <h3 class="text-center display-4">TILE 3</h3> -->
+          <img src="http://2kg-design.local/wp-content/uploads/2018/12/branding.jpg" alt="" class="img-res shadow text-overlay__image">
         </div>
+        <div class="text-overlay__overlay overlayLeft">
+            <div class="text-overlay__text text-center"><?php the_title(); ?></div>
+          </div>
       </div>
       <div class="col-md mx-auto services__tile-4">
-        <div class="container pt-5">
-          <h3 class="text-center display-4">TILE 4</h3>
-           
+        <div class="text-overlay__container">
+          <!-- <h3 class="text-center display-4">TILE 4</h3> -->
+          <img src="http://2kg-design.local/wp-content/uploads/2018/12/branding.jpg" alt="" class="img-res shadow text-overlay__image">
         </div>
+        <div class="text-overlay__overlay overlayLeft">
+            <div class="text-overlay__text text-center"><?php the_title(); ?></div>
+          </div>
       </div>
       
     </div> 
