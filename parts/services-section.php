406ac494f54a65b597c5ca627c594296dd4a6e7b
devbysalas <rgkalin@gmail.com>
2018-12-27 20:56:54 -0800

    Tiles working way better now

--- parts/services-section.php
+++ parts/services-section.php
@@ -14,55 +14,94 @@ if (!defined('ABSPATH')) {
 <!-- about-section -->
 
 <section id="contact-section">
-  <div class="container-fluid">
+  <!-- <div class="container">
     
     <div class="row">
       
-      <div class="col-md mx-auto services__tile-1">
+      <div class="col-md services__tile">
         <div class="text-overlay__container">
-          <!-- <h3 class="text-center display-4">TILE 1</h3> -->
+          
           <img src="http://2kg-design.local/wp-content/uploads/2018/12/branding.jpg" alt="" class="img-res shadow text-overlay__image">
         </div>
         <div class="text-overlay__overlay overlayLeft">
-            <div class="text-overlay__text text-center"><?php the_title(); ?></div>
+            <div class="text-overlay__text text-center"><i class="fa fa-eye fa-4x"></i><br><p class="h1">Branding</p></div>
           </div>
       </div>
-      <div class="col-md mx-auto services__tile-2">
+      <div class="col-md services__tile">
         <div class="text-overlay__container">
-          <!-- <h3 class="text-center display-4">TILE 2</h3> -->
-          <img src="http://2kg-design.local/wp-content/uploads/2018/12/branding.jpg" alt="" class="img-res shadow text-overlay__image">
+          
+          <img src="http://2kg-design.local/wp-content/uploads/2018/12/marketing.jpg" alt="" class="img-res shadow text-overlay__image">
         </div>
         <div class="text-overlay__overlay overlayLeft">
-            <div class="text-overlay__text text-center"><?php the_title(); ?></div>
+        <div class="text-overlay__text text-center"><i class="fa fa-bar-chart fa-4x"></i><br><p class="h1">Digital Marketing</p></div>
           </div>
       </div>
       
     </div> 
     <div class="row">
       
-      <div class="col-md mx-auto services__tile-3">
+      <div class="col-md services__tile">
         <div class="text-overlay__container">
-          <!-- <h3 class="text-center display-4">TILE 3</h3> -->
-          <img src="http://2kg-design.local/wp-content/uploads/2018/12/branding.jpg" alt="" class="img-res shadow text-overlay__image">
+          
+          <img src="http://2kg-design.local/wp-content/uploads/2018/12/video.jpg" alt="" class="img-res shadow text-overlay__image">
         </div>
         <div class="text-overlay__overlay overlayLeft">
-            <div class="text-overlay__text text-center"><?php the_title(); ?></div>
+        <div class="text-overlay__text text-center"><i class="fa fa-play-circle fa-4x"></i><br><p class="h1">Video</p></div>
           </div>
       </div>
-      <div class="col-md mx-auto services__tile-4">
+      <div class="col-md services__tile">
         <div class="text-overlay__container">
-          <!-- <h3 class="text-center display-4">TILE 4</h3> -->
-          <img src="http://2kg-design.local/wp-content/uploads/2018/12/branding.jpg" alt="" class="img-res shadow text-overlay__image">
+          
+          <img src="http://2kg-design.local/wp-content/uploads/2018/12/coding.jpg" alt="" class="img-res shadow text-overlay__image">
         </div>
         <div class="text-overlay__overlay overlayLeft">
-            <div class="text-overlay__text text-center"><?php the_title(); ?></div>
+        <div class="text-overlay__text text-center"><i class="fa fa-television fa-4x"></i><br><p class="h1">Web Design</p></div>
           </div>
       </div>
       
     </div> 
     
      
+  </div> -->
+
+  <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+    <div class="mr-md-3 text-overlay__container">
+      <img src="http://2kg-design.local/wp-content/uploads/2018/12/branding.jpg" alt="" class="img-res shadow text-overlay__image">
+      <div class="mr-md-3 text-center text-white overflow-hidden">
+        <div class="text-overlay__overlay overlayLeft">
+          <div class="text-overlay__text text-center"><i class="fa fa-television fa-4x"></i><br><p class="h1">Web Design</p></div>
+        </div>
+      </div>
+    </div>
+    <div class="mr-md-3 text-overlay__container">
+      <img src="http://2kg-design.local/wp-content/uploads/2018/12/branding.jpg" alt="" class="img-res shadow text-overlay__image">
+      <div class="mr-md-3 text-center text-white overflow-hidden">
+        <div class="text-overlay__overlay overlayLeft">
+          <div class="text-overlay__text text-center"><i class="fa fa-television fa-4x"></i><br><p class="h1">Web Design</p></div>
+        </div>
+      </div>
+    </div>
   </div>
+
+  <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+    <div class="mr-md-3 text-overlay__container">
+      <img src="http://2kg-design.local/wp-content/uploads/2018/12/branding.jpg" alt="" class="img-res shadow text-overlay__image">
+      <div class="mr-md-3 text-center text-white overflow-hidden">
+        <div class="text-overlay__overlay overlayLeft">
+          <div class="text-overlay__text text-center"><i class="fa fa-television fa-4x"></i><br><p class="h1">Web Design</p></div>
+        </div>
+      </div>
+    </div>
+    <div class="mr-md-3 text-overlay__container">
+      <img src="http://2kg-design.local/wp-content/uploads/2018/12/branding.jpg" alt="" class="img-res shadow text-overlay__image">
+      <div class="mr-md-3 text-center text-white overflow-hidden">
+        <div class="text-overlay__overlay overlayLeft">
+          <div class="text-overlay__text text-center"><i class="fa fa-television fa-4x"></i><br><p class="h1">Web Design</p></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
 
 
 </section>
