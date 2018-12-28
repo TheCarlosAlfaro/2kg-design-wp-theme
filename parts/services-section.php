1c62a163f90070e0428ab2a6f28e3025126d3ecc
devbysalas <rgkalin@gmail.com>
2018-12-27 22:15:34 -0800

    Added hover effect to tiles, services section

--- parts/services-section.php
+++ parts/services-section.php
@@ -11,74 +11,39 @@ if (!defined('ABSPATH')) {
 }
 ?>
 
-<!-- about-section -->
+<!-- services-section -->
 
 <section id="contact-section">
-  <!-- <div class="container">
-    
-    <div class="row">
-      
-      <div class="col-md services__tile">
-        <div class="text-overlay__container">
-          
-          <img src="http://2kg-design.local/wp-content/uploads/2018/12/branding.jpg" alt="" class="img-res shadow text-overlay__image">
-        </div>
-        <div class="text-overlay__overlay overlayLeft">
-            <div class="text-overlay__text text-center"><i class="fa fa-eye fa-4x"></i><br><p class="h1">Branding</p></div>
-          </div>
-      </div>
-      <div class="col-md services__tile">
-        <div class="text-overlay__container">
-          
-          <img src="http://2kg-design.local/wp-content/uploads/2018/12/marketing.jpg" alt="" class="img-res shadow text-overlay__image">
-        </div>
-        <div class="text-overlay__overlay overlayLeft">
-        <div class="text-overlay__text text-center"><i class="fa fa-bar-chart fa-4x"></i><br><p class="h1">Digital Marketing</p></div>
-          </div>
-      </div>
-      
-    </div> 
-    <div class="row">
-      
-      <div class="col-md services__tile">
-        <div class="text-overlay__container">
-          
-          <img src="http://2kg-design.local/wp-content/uploads/2018/12/video.jpg" alt="" class="img-res shadow text-overlay__image">
-        </div>
-        <div class="text-overlay__overlay overlayLeft">
-        <div class="text-overlay__text text-center"><i class="fa fa-play-circle fa-4x"></i><br><p class="h1">Video</p></div>
-          </div>
-      </div>
-      <div class="col-md services__tile">
-        <div class="text-overlay__container">
-          
-          <img src="http://2kg-design.local/wp-content/uploads/2018/12/coding.jpg" alt="" class="img-res shadow text-overlay__image">
-        </div>
-        <div class="text-overlay__overlay overlayLeft">
-        <div class="text-overlay__text text-center"><i class="fa fa-television fa-4x"></i><br><p class="h1">Web Design</p></div>
-          </div>
-      </div>
-      
-    </div> 
-    
-     
-  </div> -->
 
   <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
     <div class="mr-md-3 text-overlay__container">
       <img src="http://2kg-design.local/wp-content/uploads/2018/12/branding.jpg" alt="" class="img-res shadow text-overlay__image">
       <div class="mr-md-3 text-center text-white overflow-hidden">
-        <div class="text-overlay__overlay overlayLeft">
-          <div class="text-overlay__text text-center"><i class="fa fa-eye fa-2x"></i><br><p class="h1">Branding</p></div>
-        </div>
+        
+          <div class="text-overlay__overlay overlayLeft">
+            <div class="text-overlay__text text-center">
+              <i class="fa fa-eye fa-3x"></i>
+              <br>
+              <p class="h3">Branding</p>
+              <a href="#contact-section" class="btn btn-light mb-3 shadow-none">Learn More</a>
+            </div>
+          </div>
+        
       </div>
     </div>
     <div class="mr-md-3 text-overlay__container">
       <img src="http://2kg-design.local/wp-content/uploads/2018/12/marketing.jpg" alt="" class="img-res shadow text-overlay__image">
       <div class="mr-md-3 text-center text-white overflow-hidden">
-        <div class="text-overlay__overlay overlayLeft">
-          <div class="text-overlay__text text-center"><i class="fa fa-bar-chart fa-2x"></i><br><p class="h1">Digital Marketing</p></div>
-        </div>
+        
+          <div class="text-overlay__overlay overlayLeft">
+            <div class="text-overlay__text text-center">
+              <i class="fa fa-bar-chart fa-3x"></i>
+              <br>
+              <p class="h3">Digital Marketing</p>
+              <a href="#" class="btn btn-light mb-3 shadow-none">Learn More</a>
+            </div>
+          </div>
+        
       </div>
     </div>
   </div>
@@ -87,17 +52,31 @@ if (!defined('ABSPATH')) {
     <div class="mr-md-3 text-overlay__container">
       <img src="http://2kg-design.local/wp-content/uploads/2018/12/video.jpg" alt="" class="img-res shadow text-overlay__image">
       <div class="mr-md-3 text-center text-white overflow-hidden">
-        <div class="text-overlay__overlay overlayLeft">
-          <div class="text-overlay__text text-center"><i class="fa fa-play-circle fa-2x"></i><br><p class="h1">Video</p></div>
-        </div>
+        
+          <div class="text-overlay__overlay overlayLeft">
+            <div class="text-overlay__text text-center">
+              <i class="fa fa-play-circle fa-3x"></i>
+              <br>
+              <p class="h3">Video</p>
+              <a href="#contact-section" class="btn btn-light mb-3 shadow-none">Learn More</a>
+            </div>
+          </div>
+        
       </div>
     </div>
     <div class="mr-md-3 text-overlay__container">
       <img src="http://2kg-design.local/wp-content/uploads/2018/12/coding.jpg" alt="" class="img-res shadow text-overlay__image">
       <div class="mr-md-3 text-center text-white overflow-hidden">
-        <div class="text-overlay__overlay overlayLeft">
-          <div class="text-overlay__text text-center"><i class="fa fa-television fa-2x"></i><br><p class="h1">Web Design</p></div>
-        </div>
+        
+          <div class="text-overlay__overlay overlayLeft">
+            <div class="text-overlay__text text-center">
+              <i class="fa fa-television fa-3x"></i>
+              <br>
+              <p class="h3">Web Design</p>
+              <a href="#contact-section" class="btn btn-light mb-3 shadow-none">Learn More</a>
+            </div>
+          </div>
+        
       </div>
     </div>
   </div>
